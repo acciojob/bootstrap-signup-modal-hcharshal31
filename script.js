2543fcd678a7ba1e0bec92badd3eeecc2f243e69
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-08-05 01:48:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+$(document).ready(function() {
+            var modal = $('#my-modal');
+            var btn = $('#signing');
+            var span = $('.close');
+
+            btn.on('click', function() {
+                modal.show();
+            });
+
+            span.on('click', function() {
+                modal.hide();
+            });
+
+            $(window).on('click', function(event) {
+                if (event.target == modal[0]) {
+                    modal.hide();
+                }
+            });
+        });
